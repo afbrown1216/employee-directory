7867fa80c99c6ca5cfc8798d89bf00b55a1883bd
Phantom <fay.ashbro@gmail.com>
2020-10-31 18:55:06 -0400

    created table header component breaking down the table pieces

---
+++ src/components/TableHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TableHeader = () => {
+    return (
+            <>
+            <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>DOB</th>     
+            </tr>
+            </>
+    
+    );
+};
+
+export default TableHeader;
