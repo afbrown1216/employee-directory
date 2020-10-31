7285ffe3e77af55256f2720e0c963410a0eb96db
Phantom <fay.ashbro@gmail.com>
2020-10-31 18:53:00 -0400

    Created table component with props

---
+++ src/components/Table.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import TableHeader from "./TableHeader";
+import TableBody from "./TableBody";
+import employees from "../greys.json";
+
+
+
+
+class Table extends Component {
+    //set state for greys array for the table page 
+    state = {
+        employees
+    }
+    render() {
+        return (
+            <div className= "container">
+                <div className = "row">
+                    <table className = "col-sm">
+                        <TableHeader />
+                        {this.state.employees.map((employee) => (
+                            <TableBody
+                            id ={employee.id}
+                            key={employee.id}
+                            name={employee.name}
+                            image={employee.image}
+                            phone={employee.phone}
+                            email={employee.email}
+                            dob={employee.dob}
+                            />
+                        ))}
+                    </table>
+                </div>
+            
+            </div>
+        );
+    }
+}
+
+export default Table;
+
