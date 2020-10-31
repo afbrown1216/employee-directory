e78563d7d9afca316f65a6f2ffefc6fd998104df
Phantom <fay.ashbro@gmail.com>
2020-10-31 18:55:27 -0400

    created home page component

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Table from "../components/Table";
+
+const home = () => {
+    return (
+        <div>
+
+            <Header />
+            <Search />
+            <Table /> 
+            
+        </div>
+    );
+};
+
+export default home;
