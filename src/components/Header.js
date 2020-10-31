2fed42b83a5c8deca17886a9d18672937edb870d
Phantom <fay.ashbro@gmail.com>
2020-10-31 18:52:22 -0400

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h3 class="display-4">Employee Directory </h3>
+            <p class="lead"> A list of Grey Sloan Memorial employees</p>
+        </div>
+    </div>
+    
+    );
+};
+
+export default Header;
