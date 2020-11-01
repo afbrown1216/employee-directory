2d5a10758d93191c98f6f1057de54db7d5bbf118
Phantom <fay.ashbro@gmail.com>
2020-11-01 12:07:12 -0500

    chagned Table to a functional component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TableHeader from "./TableHeader";
 import TableBody from "./TableBody";
 
-
-
-
-
-class Table extends Component {
-    render() {
-      return (
-        <div className='container'>
-          <div className='row'>
-            <table className='col-sm'>
-              <tbody>
-                <TableHeader />
-                {this.props.employees.map((employee) => (
-                  <TableBody
-                    id={employee.id}
-                    key={employee.id}
-                    name={employee.name}
-                    image={employee.image}
-                    phone={employee.phone}
-                    email={employee.email}
-                    dob={employee.dob}
-                  />
-                ))}
-              </tbody>
-            </table>
-          </div>
+const Table = (props) => {
+    return (
+        <div>
+            <div className='container'>
+                <div className='row'>
+                    <table className='col-sm'>
+                        <tbody>
+                            <TableHeader />
+                                {this.props.employees.map((employee) => (
+                                <TableBody
+                                    id={employee.id}
+                                    key={employee.id}
+                                    name={employee.name}
+                                    image={employee.image}
+                                    phone={employee.phone}
+                                    email={employee.email}
+                                    dob={employee.dob}
+                                />
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            
         </div>
-      );
-    }
-  }
+    );
+};
 
 export default Table;
-
